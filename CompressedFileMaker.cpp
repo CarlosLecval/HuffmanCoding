c037ccf5d4a8e4ad82cc9c6a439d34cd4943f4de
Jyaru Hernandez <jyaru1110@gmail.com>
2022-11-09 15:12:44 -0600

    CompressedFileMaker

---
+++ CompressedFileMaker.cpp
@@ -0,0 +1,57 @@
+#include "CompressedFileMaker.h"
+CompressedFileMaker::CompressedFileMaker(string fileName,vector <char> letters, vector <string> codes)
+{
+    this->fileName = fileName;
+    this->size = letters.size();
+    this->letters = letters;
+    this->codes = codes;
+}
+
+void CompressedFileMaker::encode()
+{
+    for (int i = 0; i < text.size(); i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (text[i] == letters[j])
+            {
+                enconded_text += codes[j];
+            }
+        }
+    }
+}
+
+void CompressedFileMaker::openAndGetFile()
+{
+    ifstream file;
+    file.open(fileName);
+    if (file.is_open())
+    {
+        string line;
+        while (getline(file, line))
+        {
+            text += line;
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+    //cout << text << endl;
+}
+
+void CompressedFileMaker::openAndWriteFile()
+{
+    ofstream file;
+    file.open(fileName);
+    file << enconded_text;
+    file.close();
+}
+
+void CompressedFileMaker::make_encoded_file()
+{
+    openAndGetFile();
+    encode();
+    openAndWriteFile();
+}
