0e2cee2a102b5e37e4b6ada8891f7438f163a525
Carloslecval <carloslecval03@gmail.com>
2022-11-10 18:08:20 -0600

    Huffman tree fixed

--- Compress/HuffmanTree.cpp
+++ Compress/HuffmanTree.cpp
@@ -7,15 +7,39 @@ HuffmanTree::HuffmanTree(int size) : pq(size)
 
 void HuffmanTree::buildTree()
 {
-    while (pq.getSize() > 1)
+    while (pq.getSize() > 0)
     {
 
         Node *left = pq.pop();
         Node *right = pq.pop();
+        cout << "left: " << left->frequency << " right: " << right->frequency << endl;
         Node *parent = new Node('#', left->frequency + right->frequency);
         parent->left = left;
         parent->right = right;
         pq.push(parent);
+        //pq.print();
     }
     root = pq.pop();
+}
+
+void HuffmanTree::printTree(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printTree(node->left);
+    cout << node->character << " " << node->frequency << endl;
+    printTree(node->right);
+}
+
+void HuffmanTree::buildPriorityQueue()
+{
+    /*pq.push(new Node('f', 45));
+    pq.push(new Node('e', 16));
+    pq.push(new Node('d', 13));
+    pq.push(new Node('c', 12));
+    pq.push(new Node('b', 9));
+    pq.push(new Node('a', 5));
+    pq.print();*/
 }
