c8513af6aef1fa27cf128ad3ca6883cdb0b8f733
Jyaru Hernandez <jyaru1110@gmail.com>
2022-11-12 00:12:18 -0600

    write binary code

--- Compress/HuffmanTree.cpp
+++ Compress/HuffmanTree.cpp
@@ -3,6 +3,7 @@
 HuffmanTree::HuffmanTree(int size) : pq(size)
 {
     root = NULL;
+    codigoGeneral = "";
 }
 
 void HuffmanTree::buildTree()
@@ -35,11 +36,97 @@ void HuffmanTree::printTree(Node *node)
 
 void HuffmanTree::buildPriorityQueue()
 {
-    /*pq.push(new Node('f', 45));
+    pq.push(new Node('f', 45));
     pq.push(new Node('e', 16));
     pq.push(new Node('d', 13));
     pq.push(new Node('c', 12));
     pq.push(new Node('b', 9));
     pq.push(new Node('a', 5));
-    pq.print();*/
+    pq.print();
+}
+
+void HuffmanTree::CreacionCodigos(Node* raiz)
+{
+  
+    //cout << "codigoGeneral: " << codigoGeneral << endl;
+    if (raiz == NULL){
+        cout << "entro";
+        codigoGeneral.pop_back();
+        return;
+    }
+    if(raiz->left == NULL && raiz->right == NULL){
+        Caracteres1.codigo.push_back(codigoGeneral);
+        Caracteres1.caracter.push_back(raiz->character);
+        codigoGeneral.pop_back();
+        return;
+    }
+    if(raiz->left != NULL){
+        codigoGeneral.push_back('0');
+        CreacionCodigos(raiz->left);
+    }
+    if(raiz->right != NULL){
+        codigoGeneral.push_back('1');
+        CreacionCodigos(raiz->right);
+    }
+    codigoGeneral.pop_back();
+}
+FrecuenciaCaracter HuffmanTree::Frecuencias(string archivo)
+{
+    string texto;
+    int cont = -1;
+    bool esta = false;
+    ifstream MyReadFile(archivo);
+    while (getline (MyReadFile, texto)) 
+    {
+        for (int i=0; i<texto.size(); i++){
+            esta = false;
+            for (int j=0; j< Caracteres.caracter.size(); j++){
+                if (Caracteres.caracter[j] == texto[i]){
+                    Caracteres.frecuencia[j] += 1;
+                    esta = true;
+                }
+            }
+            if (esta == false){
+                Caracteres.caracter.push_back(texto[i]);
+                Caracteres.frecuencia.push_back(1);
+            }
+        }
+        if(cont != -1)
+        {
+            saltoLinea();
+        }
+        cont++;
+    }
+    for (int j=0; j< Caracteres.caracter.size(); j++){
+        cout << Caracteres.caracter[j] << "\t" << Caracteres.frecuencia[j] << endl;
+    }
+    return Caracteres;
+    MyReadFile.close();
+}
+
+void HuffmanTree::saltoLinea(){
+    bool esta = false;
+    for (int j=0; j< Caracteres.caracter.size(); j++)
+    {
+        if (Caracteres.caracter[j] == '\\'){
+            Caracteres.frecuencia[j] += 1;
+            esta = true;
+        }
+    }
+    if (esta == false){
+        Caracteres.caracter.push_back('\\');
+        Caracteres.frecuencia.push_back(1);
+    }
+    esta = false;
+    for (int j=0; j< Caracteres.caracter.size(); j++)
+    {
+        if (Caracteres.caracter[j] == 'n'){
+            Caracteres.frecuencia[j] += 1;
+            esta = true;
+        }
+    }
+    if (esta == false){
+        Caracteres.caracter.push_back('n');
+        Caracteres.frecuencia.push_back(1);
+    }
 }
--- Ivan/Source.cpp
+++ Ivan/Source.cpp
@@ -1,14 +1,31 @@
 #include "ReconstructedFile.h"
+#include "binaryIn.h"
 
 int main()
 {
-    char characterArray[7] = {'a', 'b', 'c', 'd', 'e', 'f', ' '};
-    string codeArray[7] = {"1100", "1101", "100", "101", "111", "00", "01"};
+    char characterArray[7] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    string codeArray[7] = {"1100", "1101", "100", "101", "111", "0"};
     string exampleText = "0011000100101";
     int arraySize = sizeof(characterArray) / sizeof(characterArray[0]);
 
+    BinaryStdIn binaryStdIn("encoded.dat");
+
+    string binaryCode = "";
+    
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+    binaryCode += binaryStdIn.readBool() ? "1" : "0";
+
+    cout << binaryCode << endl;
+
     ReconstructedFile decompressedFile(characterArray, codeArray, arraySize);
-    string text = decompressedFile.decompress(exampleText);
+    string text = decompressedFile.decompress(binaryCode);
     cout << text << endl;
     decompressedFile.openAndWriteFile(text);
 
